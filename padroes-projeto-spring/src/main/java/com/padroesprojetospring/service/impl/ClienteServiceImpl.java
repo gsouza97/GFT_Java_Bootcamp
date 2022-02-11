8c7ce0eb86c9f1c4211cac9bdf2c6347f79e6fbd
Gabriel Souza <gabrielss_@outlook.com>
2022-02-10 21:43:08 -0300

    padrao de projeto com spring

---
+++ padroes-projeto-spring/src/main/java/com/padroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,60 @@
+package com.padroesprojetospring.service.impl;
+
+import java.util.Optional;
+
+import com.padroesprojetospring.model.Cliente;
+import com.padroesprojetospring.model.ClienteRepository;
+import com.padroesprojetospring.model.Endereco;
+import com.padroesprojetospring.model.EnderecoRepository;
+import com.padroesprojetospring.service.ClienteService;
+import com.padroesprojetospring.service.ViaCepService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+        salvarClienteComCep(cliente);
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+
+    private void salvarClienteComCep(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+    }
+
+}
